30ee57296a713c20e2dcaffb5878edf4723b5d22
LeeYatyi <569894828@qq.com>
2020-03-08 21:57:22 +0800

    add unittest

--- test_mubu_login.py
+++ test_mubu_login.py
@@ -1,13 +1,16 @@
 import requests
+import unittest
 
 
-def test_get_homepage():
-    url = "https://mubu.com"
-    headers = {
-        "user - agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36"
-    }
-    resp = requests.get(url, headers=headers, verify=False)
-    assert resp.status_code == 200
+class TestMubuLogin(unittest.TestCase):
+
+    def test_get_homepage(self):
+        url = "https://mubu.com"
+        headers = {
+            "user - agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36"
+        }
+        resp = requests.get(url, headers=headers, verify=False)
+        assert resp.status_code == 200
 
 if __name__ == '__main__':
-    test_get_homepage()
+    TestMubuLogin().test_get_homepage()
