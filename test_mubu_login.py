8f276023f899c26f775e876a11075ee97a25c0e2
LeeYatyi <569894828@qq.com>
2020-03-08 19:25:13 +0800

    init with poetry

---
+++ test_mubu_login.py
@@ -0,0 +1,13 @@
+import requests
+
+
+def test_get_homepage():
+    url = "https://mubu.com"
+    headers = {
+        "user - agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36"
+    }
+    resp = requests.get(url, headers=headers, verify=False)
+    assert resp.status_code == 200
+
+if __name__ == '__main__':
+    test_get_homepage()
